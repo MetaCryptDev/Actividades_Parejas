73cc9d235cadd260ece792556d5cb4a5f0425a41
Alberto Pinilla Gonzalez-Mohino <116631543+MetaCryptDev@users.noreply.github.com>
2023-04-18 11:08:16 +0200

    Add files via upload

--- src/Paquete/Divisible.java
+++ src/Paquete/Divisible.java
@@ -1,9 +1,21 @@
 package Paquete;
 
+import java.util.Scanner;
+
 public class Divisible {
 
+	static Scanner sc = new Scanner (System.in);
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+	System.out.println("Dime el ultimo numero");
+	int numUltimo = sc.nextInt();
+	
+	for (int i = 1; i < numUltimo; i++) {
+		if(i % 2 == 0 || i % 3 == 0) {
+			System.out.println(i);
+		}
+		
+	}
 
 	}
 
